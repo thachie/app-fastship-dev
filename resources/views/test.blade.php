1fa7b75c3f2834e32ad2a18fa47ca0ab98d84291
Thachie <sadsydogz@hotmail.com>
2020-02-18 17:00:29 +0700

    delete unused view

--- resources/views/test.blade.php
+++
@@ -1,9 +0,0 @@
-<form action="{{ url('liff/action/login2') }}" method="post">
-	<input type="text" name="line_user_id" value="U2ee50e8b0e3b152688532c3fb6573fd0" />
-	<input type="text" name="line_id" value="Supotjanee" />
-	<input type="text" name="return" value="" />
-	<input type="text" name="email" value="pink3579@hotmail.com" />
-	<input type="text" name="password" value="sriinkia2401" />
-	
-	<input type="submit" value="submit" />
-</form>
