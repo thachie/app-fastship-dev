88bd28a31b080b23d59df322e4e1a7dbb5182172
Thachie <sadsydogz@hotmail.com>
2020-02-25 11:08:29 +0700

    Payment Completed

--- app/Http/Controllers/Payment/PaymentController.php
+++ app/Http/Controllers/Payment/PaymentController.php
@@ -66,77 +66,28 @@ class PaymentController extends Controller
     
     public function paymentCompleted(Request $request)
     {
+        
+        //get parameters
         $chargeId = $request->input('chargeId');
         $token = $request->input('token');
         $paymentMethods = $request->input('paymentMethods');
 
-        /*
-        if ($paymentMethods = "card") {
-        	
-        	//5431289719925031 MASTERCARD
-        	//4024007134786226 VISA
-        	//3536353014086177 JCB
-        	$amount = "1";
-        	$reference_order = date("mdHis").substr($_SERVER['REQUEST_TIME'],-3);
-        	$jsonCardCreateCharge = '{
-				"amount": '.$amount.',
-				"currency": "THB",
-				"description": "TESTCARDSANDBOX",
-				"source_type": "card",
-				"mode": "token",
-				"token": "'.$token.'",
-				"reference_order" : "'.$reference_order.'",
-				"ref_1": "ref1",
-				"ref_2": "123456"
-			}';
-			alert($jsonCardCreateCharge);
-        	$method = "POST";
-	        $url = "https://kpaymentgateway-services.kasikornbank.com/card/v2/charge";
-	        $jsonData = $jsonCardCreateCharge;
-	        alert($url);
-	        $response = callAPI_Kbank($method, $url, $jsonData);
-	        $transaction = json_decode($response, true);
-            alert($response);
-            alert($transaction);
-	        $id = $transaction['id'];
-	        $transaction_state = $transaction['transaction_state'];
-	        $status = $transaction['status'];
-	        $redirect_url = $transaction['redirect_url'];
-	        if ($transaction_state == 'Pre-Authorized' && $status == 'success') {
-	        	return redirect($redirect_url);
-	        	exit();
-	        }elseif ($transaction_state == 'Authorized' && $status == 'success') {
-	        	//alert('Card Charge Success');
-	        	//alert($transaction);
-	        	$completed = ($transaction['status'] == $transaction['status'] && $transaction['transaction_state'] == $transaction['transaction_state']);
-	        	$transaction_state = $transaction['transaction_state'];
-	        	$pickupId = $transaction['reference_order'];
-	        }else{
-	        	//alert('Card Charge Fail');
-	        	//alert($transaction);
-	        	$completed = ($transaction['status'] == $transaction['status'] && $transaction['transaction_state'] == $transaction['transaction_state']);
-	        	$transaction_state = $transaction['transaction_state'];
-	        	$pickupId = $transaction['reference_order'];
-	        }
-        }else{
-        */
-        	$method = "GET";
-	        $url = "https://kpaymentgateway-services.kasikornbank.com/qr/v2/qr/".$chargeId;
-	        $jsonData = "";
-	        
-	        $response = callAPI_Kbank($method, $url, $jsonData);
-	        $transaction = json_decode($response, true);
-	        //alert($transaction);
-	        
-	        $LOGPROCESS = storage_path('logs/kbank_payment_completed.log');
-	        $this->GEN_Logs(date("Y-m-d H:i:s")."|$chargeId|$response",$LOGPROCESS,'l' );
-	        
-	        $completed = ($transaction['status'] == "success" && $transaction['transaction_state'] == "Authorized");
-	        $transaction_state = $transaction['transaction_state'];
-	        $pickupId = $transaction['reference_order'];
-	        
-       // }
-       
+        //get charge detail
+    	$method = "GET";
+        $url = "https://kpaymentgateway-services.kasikornbank.com/qr/v2/qr/".$chargeId;
+        $jsonData = "";
+        
+        $response = callAPI_Kbank($method, $url, $jsonData);
+        $transaction = json_decode($response, true);
+
+        $LOGPROCESS = storage_path('logs/kbank_payment_completed.log');
+        $this->GEN_Logs(date("Y-m-d H:i:s")."|$chargeId|$response",$LOGPROCESS,'l' );
+        
+        //check completed
+        $completed = ($transaction['status'] == "success" && $transaction['transaction_state'] == "Authorized");
+        $transaction_state = $transaction['transaction_state'];
+        $pickupId = $transaction['reference_order'];
+
         $data = array(
             "completed" => $completed,
             "transaction_state" => $transaction_state,
@@ -145,30 +96,6 @@ class PaymentController extends Controller
         return view('payment_completed',$data);
         
     }
-
-    public function paymentCompleted2(Request $request)
-    {
-        alert($request->all());
-        $req = json_encode($request->all());
-        $LOGPROCESS = storage_path('logs/kbank_payment_status_qr.log');
-        $str = 'Start Payment Status QR';
-        $this->GEN_Logs(date("Y-m-d H:i:s")."|$str|",$LOGPROCESS,'l' );
-        $this->GEN_Logs(date("Y-m-d H:i:s")."|Request|$req",$LOGPROCESS,'l' );
-        $str = 'End Payment Status QR';
-        $this->GEN_Logs(date("Y-m-d H:i:s")."|$str|",$LOGPROCESS,'l' );
-    }
-
-    /*
-    public function paymentStatusQr(Request $request)
-    {
-        $req = json_encode($request->all());
-        $LOGPROCESS = storage_path('logs/kbank_payment_status_qr.log');
-        $str = 'Start Payment Status QR';
-        $this->GEN_Logs(date("Y-m-d H:i:s")."|$str|",$LOGPROCESS,'l' );
-        $this->GEN_Logs(date("Y-m-d H:i:s")."|Request|$req",$LOGPROCESS,'l' );
-        $str = 'End Payment Status QR';
-        $this->GEN_Logs(date("Y-m-d H:i:s")."|$str|",$LOGPROCESS,'l' );
-    }*/
 
     public function paymentStatusCard(Request $request)
     {
@@ -239,45 +166,6 @@ class PaymentController extends Controller
         }else{
             alert('Payment Methods or Charge ID or Order ID is null!');
         }
-    }
-
-    public function prepareQrTest($code,Request $request)
-    {
-        $customerId = 4815;
-        $amount = number_format($code,2);
-        $req = $code;
-        $shipment_id = "151234567";
-        $description = "Test Payment QR";
-
-        $shipment_id = '15'.date("mdHis").substr($_SERVER['REQUEST_TIME'],-3);
-        $jsonCreateOrderId = '{
-            "amount": '.$amount.',
-            "currency": "THB",
-            "description": "'.$description.'",
-            "source_type": "qr",
-            "reference_order": "'.$shipment_id.'"
-        }';
-        $method = "POST";
-        $url = "https://kpaymentgateway-services.kasikornbank.com/qr/v2/order";
-        $jsonData = $jsonCreateOrderId;
-        $response = callAPI_Kbank($method, $url, $jsonData);
-        $res = json_decode($response, true);
-        $order_id = $res['id'];
-        //$payment_methods = 'qr';
-        $payment_methods = 'card';
-        //$payment_methods = 'redirect';
-
-        $LOGPROCESS = storage_path('logs/kbank_create_order.log');
-        $this->GEN_Logs(date("Y-m-d H:i:s")."|Response|$response",$LOGPROCESS,'l' );
-        $data = array(
-            "amount" => $amount,
-            "order_id" => $order_id,
-            "shipment_id" => $shipment_id,
-            "customerId" => $customerId,
-            "payment_methods" => $payment_methods,
-            "description" => $description,
-        );
-        return view('payment_qr1',$data);
     }
 
     public function prepareUnionPay($code,Request $request)
